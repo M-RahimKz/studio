a91d42a2d1f8a9665583cf77fe7f819c1a908b89
WSI Account <wsia26119@gmail.com>
2025-10-31 16:14:30 +0000

    getting error in case of File size greater then 1 mb
     POST / 200 in 478

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  // Increasing body size limit for server actions to 4MB
   serverActions: {
     bodySizeLimit: '4mb',
   },
